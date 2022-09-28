471febd0d84f82f9445a7d0b1af480475e1e06a2
MahsaSnt <57616761+MahsaSnt@users.noreply.github.com>
2022-09-28 15:24:48 +0330

    Delete coding_challenge_project directory

--- coding_challenge_project/taskam/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from .views import ProjectView, TaskView
-
-router = routers.DefaultRouter()
-router.register(r'project', ProjectView)
-router.register(r'task', TaskView)
-
-urlpatterns = router.urls
